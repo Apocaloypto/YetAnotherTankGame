242b195fd39477b817260cf1a317b70274af10fc
Apocaloypto <51018969+Apocaloypto@users.noreply.github.com>
2021-12-22 19:51:55 +0100

    Sachen

--- YetAnotherTankGame/Main.cpp
+++ YetAnotherTankGame/Main.cpp
@@ -4,12 +4,22 @@
 #include "Logging.h"
 #include "Window.h"
 
+#ifdef DEBUG_MODE
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+#endif
+
 
 using namespace std;
 
 int main(int argc, char **argv)
 {
    static_assert(sizeof(void *) == 8, "Enable 64-Bit-Support!");
+
+#ifdef DEBUG_MODE
+   _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
 
    GENERAL_LOG.Log(LogType::Message, "Starting game...");
 
